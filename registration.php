b293721e3972e0446bdee7de153af679d3969ecc
Paul Briscoe <briscoe.paul@gmail.com>
2017-07-28 07:45:17 -0400

    Update registration.php

--- registration.php
+++ registration.php
@@ -1,10 +1,10 @@
 <?php
 /**
-* @package     
-* @version
-* @author      Human-Element, Inc. <info@human-element.com>
-* @copyright   Copyright 2017 Human-Element, Inc.
-**/
+ * Human-Element Inc.
+ *
+ * @package HumanElement_Base
+ * @copyright Copyright (c) 2017 Human Element Inc. (http://www.human-element.com)
+ */
 
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
