85c250c0d5f74d26b6c75f8c7d7dd2aaec368424
Paul Briscoe <briscoe.paul@gmail.com>
2017-07-28 07:43:47 -0400

    Update registration.php
    We should also probably include a standard copyright info on all php files where we can

--- registration.php
+++ registration.php
@@ -1,4 +1,10 @@
 <?php
+/**
+* @package     
+* @version
+* @author      Human-Element, Inc. <info@human-element.com>
+* @copyright   Copyright 2017 Human-Element, Inc.
+**/
 
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
